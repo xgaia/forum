936185e1db846bbcb5d4ad3cfea49820850409fe
xgaia <xgaia@gmx.com>
2021-06-07 21:32:03 +0200

    add forum app

---
+++ makinaforum/forum/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Thread(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="threads")
+    public = models.BooleanField(default=False)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name="messages")
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
