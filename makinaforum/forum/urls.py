9884ada98a9a586f4bed9dcdcc53af837511e54f
xgaia <xgaia@gmx.com>
2021-06-07 22:30:40 +0200

    use generic view + base template

--- makinaforum/forum/urls.py
+++ makinaforum/forum/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'forum'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('thread/<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- makinaforum/forum/views.py
+++ makinaforum/forum/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Thread, Message
+
+
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'threads'
+
+    def get_queryset(self):
+        return Thread.objects.order_by('-date')
+
+
+class DetailView(generic.DetailView):
+    model = Thread
+    template_name = 'detail.html'
+
+
+
